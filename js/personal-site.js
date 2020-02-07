17a041a38946ddc26fce69d7cd636c51075d1bb5
Carlos Teller <carlos.z.teller@gmail.com>
2020-02-07 12:16:51 -0600

    Made some modifications to my personal site as well as created a JS file

---
+++ js/personal-site.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function() {
+    // Get the current year;
+    function getCurrentYear() {
+        var currentDate = new Date(Date.now());
+
+        return currentDate.getFullYear();
+    }
+})();
