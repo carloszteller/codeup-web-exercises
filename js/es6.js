7ec542d39672697784556ffe448c58937707fd51
Carlos Teller <carlos.z.teller@gmail.com>
2020-03-10 11:37:00 -0500

    Modified to clean up code for better readability

--- js/es6.js
+++ js/es6.js
@@ -51,12 +51,8 @@ let names = [];
 // users.forEach(function(user) {
 //   return names.push(user.name);
 // });
-users.forEach((user) => {
-  emails.push(user.email);
-});
-users.forEach((user) => {
-  names.push(user.name);
-});
+users.forEach(user => emails.push(user.email));
+users.forEach(user => names.push(user.name));
 
 
 // TODO: replace `var` with `let` in the following declaration
