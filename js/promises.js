2d5eddf837931d31a21596a0bb13661ef3bcc6ce
Carlos Teller <carlos.z.teller@gmail.com>
2020-03-11 15:27:37 -0500

    Completed the JS Promise exercise

---
+++ js/promises.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const wait = time => {
+    return new Promise(resolve => {
+       setTimeout(resolve, time);
+    });
+}
+
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+// const user_commit = username => {
+//     return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': githubToken}}).then(data => {
+//         return fetch(data[data.length - 1].url, {headers: {'Authorization': githubToken}}).then(response => response.json());
+//     });
+// }
+const user_commit = username => {
+    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': githubToken}}).then(response => response.json()).then(response => {
+        console.log(response);
+        let commits = response[response.length - 1].payload.commits;
+        return fetch(commits[commits.length - 1].url, {headers: {'Authorization': githubToken}}).then(response => response.json());
+    });
+}
+
+user_commit('carloszteller').then(data => {
+    console.log(new Date(data.commit.author.date));
+}).catch(error => {
+    console.log(error);
+});
+
