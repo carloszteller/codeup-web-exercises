f38f3faa98379db6520d302ecc9b8c364d88b6f7
Carlos Teller <carlos.z.teller@gmail.com>
2020-04-23 10:32:20 -0500

    Changed the skills section to look like a code editor using CodeMirror

--- js/personal-site.js
+++ js/personal-site.js
@@ -1,6 +1,14 @@
 "use strict";
 
 (function() {
+    // Codemirror editor
+    let html = CodeMirror.fromTextArea($('#skills')[0],  {
+        mode: 'xml',
+        lineNumbers: true,
+        lineWrapping: true,
+        readOnly: true
+    });
+
     // Get the current year and place in year class located in the footer
     var currentDate = new Date(Date.now());
     document.getElementsByClassName('year')[0].innerText = currentDate.getFullYear();
